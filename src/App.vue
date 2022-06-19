8684302e9a3fcf05fcc20155ba1b16173a8d00d9
Lennon Bueno <lennonsbueno@gmail.com>
2022-06-19 20:34:39 -0300

    add inputs

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,74 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 import TheHeader from './components/TheHeader.vue'
+import TheInput from './components/TheInput.vue'
+
+const valor = ref('')
+const data = ref('')
+const observacao = ref('')
 </script>
 
 <template>
   <TheHeader />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
+  <div class="container mx-auto">
+    <div class="grid grid-cols-4 mt-8 gap-4 content-end grid-rows-1">
+      <div>
+        <label
+          for="txtValor"
+          class="block"
+        >
+          Valor
+        </label>
+        <TheInput
+          id="txtValor"
+          type="text"
+          v-model="valor"
+          name="txtValor"
+          placeholder="Valor:"
+          class="min-w-full"
+        />
+      </div>
+
+      <div>
+        <label
+          for="txtData"
+          class="block"
+        >
+          Data
+        </label>
+        <TheInput
+          id="txtData"
+          type="text"
+          v-model="data"
+          name="txtData"
+          placeholder="Data:"
+          class="min-w-full"
+        />
+      </div>
+
+      <div>
+        <label
+          for="txtObservacao"
+          class="block"
+        >
+          Observação
+        </label>
+        <TheInput
+          id="txtObservacao"
+          type="text"
+          v-model="observacao"
+          name="txtObservacao"
+          placeholder="Observação:"
+          class="min-w-full"
+        />
+      </div>
+
+      <button class="border border-blue-800 pt-2 py-2 min-w-full rounded-md bg-blue-600 text-white">
+        Salvar
+      </button>
+    </div>
+  </div>
 </template>
 
 <style>
---
+++ src/components/TheInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <input
+    :type="types"
+    :value="modelValue"
+    :name="name"
+    :id="id"
+    :placeholder="placeholder"
+    class="border p-1 rounded-md"
+    @input="$emit('update:modelValue', $event.target.value)"
+  >
+</template>
+
+<script setup lang="ts">
+  import { computed } from 'vue'
+
+  const props = defineProps({
+    type: { type: String, required: true, default: 'text' },
+    name: { type: String, required: false, default: '' },
+    id: { type: String, required: false, default: '' },
+    placeholder: { type: String, required: false, default: '' },
+    modelValue: { type: String, required: false, default: '' }
+  })
+
+  const typesInput = ['text', 'number']
+
+  const types = computed(() => {
+    if (typesInput.includes(props.type)) {
+      return props.type
+    }
+
+    return 'text'
+  })
+</script>
+
+<style scoped>
+
+</style>
