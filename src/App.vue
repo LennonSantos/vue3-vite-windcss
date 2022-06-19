251000e3c88041de90edf91e57de2fff80f145c1
Lennon Bueno <lennonsbueno@gmail.com>
2022-06-19 02:03:44 -0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
+  <TheHeader />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="bg-gray-200 p-2 border-b border-gray-300">
+    <div class="container mx-auto">
+      <h1 class="text-2xl">Gerenciador de contas</h1>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
